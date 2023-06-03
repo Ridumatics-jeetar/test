dc853d6081b79f6539860779d59467a889030294
Ridumatics-jeetar <ridwan.onikoyi@jeetar.com>
2023-06-03 19:37:25 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var GENDER;
+(function (GENDER) {
+    GENDER["MALE"] = "MALE";
+    GENDER["FEMALE"] = "FEMALE";
+})(GENDER || (GENDER = {}));
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+enum GENDER {
+  MALE = "MALE",
+  FEMALE = "FEMALE",
+}
